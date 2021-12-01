c64fcc8f9a849f004d5a7f316c5906dc30ac3e4a
Serg <64j@mail.ru>
2021-12-01 22:15:24 +0300

    up

---
+++ assets/tvs/multifieldsjs/multifieldsjs.customtv.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * custom tv multifields
+ * @author 64j
+ */
+
+if (!defined('MODX_BASE_PATH')) {
+    die('HACK???');
+}
+
+$id = $row['id'] ?? 0;
+$value = $row['value'] ?? '';
+$config = MODX_BASE_PATH . 'assets/plugins/multifieldsjs/config/' . $id;
+
+if (is_file($config . '.php')) {
+    $config = require $config . '.php';
+    $config = json_encode($config, JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES);
+} elseif (is_file($config . '.json')) {
+    $config = file_get_contents($config . '.json');
+} else {
+    $config = '{}';
+}
+
+echo '
+<div id="mfjs-' . $id . '" class="mfjs" data-mfjs="' . $id . '"></div>
+<textarea name="tv' . $id . '" rows="10" hidden>' . $value . '</textarea>
+<script>MfJs.initElement(\'mfjs-' . $id . '\', ' . $config . ');</script>';
