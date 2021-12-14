9803cee254ceeed139e96d185169c5ecac7a5a93
s.kochin <s.kochin@autorus>
2021-12-14 09:40:38 +0300

    fix plugin

--- assets/plugins/multifieldsjs/plugin.multifieldsjs.php
+++ assets/plugins/multifieldsjs/plugin.multifieldsjs.php
@@ -5,37 +5,34 @@
  * @author 64j
  */
 
-/** @var DocumentParser $modx */
-$e = &$modx->event;
+$evo = evolutionCMS();
 
-switch ($e->name) {
-    case 'OnManagerMainFrameHeaderHTMLBlock':
-        if (in_array($modx->manager->action, [3, 4, 17, 27, 72])) {
-            $out = [];
+if ($evo->event->name == 'OnManagerMainFrameHeaderHTMLBlock') {
+    if (in_array($evo->getManagerApi()->action, [3, 4, 17, 27, 72])) {
+        $out = [];
 
-            $elements = array_unique(array_merge([__DIR__ . '/elements/mfjs'], glob(__DIR__ . '/elements/*', GLOB_ONLYDIR)));
+        $elements = array_unique(array_merge([__DIR__ . '/elements/mfjs'], glob(__DIR__ . '/elements/*', GLOB_ONLYDIR)));
 
-            foreach ($elements as $path) {
-                if ($elements_elements = glob($path . '/*.css')) {
-                    foreach ($elements_elements as $element) {
-                        preg_match('/@version\s+(.*?)[\r]/is', file_get_contents($element, false, null, 0, 100), $matches);
-                        $version = $matches[1] ?? filemtime($element);
-                        $element = str_replace([DIRECTORY_SEPARATOR, MODX_BASE_PATH], '/', $element);
-                        $out[] = '<link rel="stylesheet" type="text/css" href="' . $element . '?v=' . $version . '"/>';
-                    }
-                }
-
-                if ($elements_elements = glob($path . '/*.js')) {
-                    foreach ($elements_elements as $element) {
-                        preg_match('/@version\s+(.*?)[\r]/is', file_get_contents($element, false, null, 0, 100), $matches);
-                        $version = $matches[1] ?? filemtime($element);
-                        $element = str_replace([DIRECTORY_SEPARATOR, MODX_BASE_PATH], '/', $element);
-                        $out[] = '<script src="' . $element . '?v=' . $version . '"></script>';
-                    }
+        foreach ($elements as $path) {
+            if ($elements_elements = glob($path . '/*.css')) {
+                foreach ($elements_elements as $element) {
+                    preg_match('/@version\s+(.*?)[\n\r]/is', file_get_contents($element, false, null, 0, 100), $matches);
+                    $version = $matches[1] ?? filemtime($element);
+                    $element = str_replace([DIRECTORY_SEPARATOR, MODX_BASE_PATH], '/', $element);
+                    $out[] = '<link rel="stylesheet" type="text/css" href="..' . $element . '?v=' . $version . '"/>';
                 }
             }
 
-            echo implode("\n", $out);
+            if ($elements_elements = glob($path . '/*.js')) {
+                foreach ($elements_elements as $element) {
+                    preg_match('/@version\s+(.*?)[\n\r]/is', file_get_contents($element, false, null, 0, 100), $matches);
+                    $version = $matches[1] ?? filemtime($element);
+                    $element = str_replace([DIRECTORY_SEPARATOR, MODX_BASE_PATH], '/', $element);
+                    $out[] = '<script src="..' . $element . '?v=' . $version . '"></script>';
+                }
+            }
         }
-        break;
+
+        echo implode("\n", $out);
+    }
 }
