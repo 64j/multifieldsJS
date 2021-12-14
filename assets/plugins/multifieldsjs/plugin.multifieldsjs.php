b496085d5c8907cbea7cec4fdca928222f4f0ba0
s.kochin <s.kochin@autorus>
2021-12-14 10:44:47 +0300

    add parse DocBlock for css and js files

--- assets/plugins/multifieldsjs/plugin.multifieldsjs.php
+++ assets/plugins/multifieldsjs/plugin.multifieldsjs.php
@@ -7,6 +7,39 @@
 
 $evo = evolutionCMS();
 
+if (!function_exists('mfJsParseDocBlock')) {
+    /**
+     * @param string $filename
+     * @return array
+     */
+    function mfJsParseDocBlock(string $filename): array
+    {
+        if (
+            is_file($filename)
+            && preg_match_all(
+                '/@(\w+)\s+(.*)\r?\n/m',
+                file_get_contents($filename, false, null, 0, 200),
+                $matches
+            )
+        ) {
+            return array_map(function ($value) {
+                switch (true) {
+                    case is_numeric($value):
+                        return is_float($value + 0) ? (float) $value : (int) $value;
+                    case $value === 'true':
+                        return true;
+                    case $value === 'false':
+                        return false;
+                    default:
+                        return (string) $value;
+                }
+            }, array_combine($matches[1], $matches[2]));
+        }
+
+        return [];
+    }
+}
+
 if ($evo->event->name == 'OnManagerMainFrameHeaderHTMLBlock') {
     if (in_array($evo->getManagerApi()->action, [3, 4, 17, 27, 72])) {
         $out = [];
@@ -16,8 +49,11 @@ if ($evo->event->name == 'OnManagerMainFrameHeaderHTMLBlock') {
         foreach ($elements as $path) {
             if ($elements_elements = glob($path . '/*.css')) {
                 foreach ($elements_elements as $element) {
-                    preg_match('/@version\s+(.*?)[\n\r]/is', file_get_contents($element, false, null, 0, 100), $matches);
-                    $version = $matches[1] ?? filemtime($element);
+                    $docBlock = mfJsParseDocBlock($element);
+                    if (!empty($docBlock['disabled'])) {
+                        continue;
+                    }
+                    $version = $docBlock['version'] ?? filemtime($element);
                     $element = str_replace([DIRECTORY_SEPARATOR, MODX_BASE_PATH], '/', $element);
                     $out[] = '<link rel="stylesheet" type="text/css" href="..' . $element . '?v=' . $version . '"/>';
                 }
@@ -25,8 +61,11 @@ if ($evo->event->name == 'OnManagerMainFrameHeaderHTMLBlock') {
 
             if ($elements_elements = glob($path . '/*.js')) {
                 foreach ($elements_elements as $element) {
-                    preg_match('/@version\s+(.*?)[\n\r]/is', file_get_contents($element, false, null, 0, 100), $matches);
-                    $version = $matches[1] ?? filemtime($element);
+                    $docBlock = mfJsParseDocBlock($element);
+                    if (!empty($docBlock['disabled'])) {
+                        continue;
+                    }
+                    $version = $docBlock['version'] ?? filemtime($element);
                     $element = str_replace([DIRECTORY_SEPARATOR, MODX_BASE_PATH], '/', $element);
                     $out[] = '<script src="..' . $element . '?v=' . $version . '"></script>';
                 }
