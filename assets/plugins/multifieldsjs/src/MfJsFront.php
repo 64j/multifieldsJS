e44587ebfebaea299edd016e73b0c367eba3b25f
Serg <64j@mail.ru>
2021-12-21 17:37:48 +0300

    added snippet

--- assets/plugins/multifieldsjs/src/MfJsFront.php
+++ assets/plugins/multifieldsjs/src/MfJsFront.php
@@ -25,6 +25,29 @@ class MfJsFront
      */
     public function render(array $params): string
     {
-        return '';
+        $tv = evolutionCMS()->documentObject[$params['tvName'] ?? null] ?? null;
+        $api = $params['api'] ?? null;
+
+        if (is_null($tv)) {
+            return '';
+        }
+
+        switch ($api) {
+            case '1':
+            case 'json':
+                return $tv[1];
+
+            default:
+                return $this->renderData(json_decode($tv[1] ?: '{}'));
+        }
+    }
+
+    /**
+     * @param array $data
+     * @return string
+     */
+    protected function renderData(array $data): string
+    {
+        dd($data);
     }
 }
