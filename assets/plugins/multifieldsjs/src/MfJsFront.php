ed4db7ae87c3dd5983d731a8cf76e34fca0b1725
Serg <64j@mail.ru>
2021-12-24 13:36:52 +0300

    fix

--- assets/plugins/multifieldsjs/src/MfJsFront.php
+++ assets/plugins/multifieldsjs/src/MfJsFront.php
@@ -8,6 +8,11 @@ class MfJsFront
     private static $instance = null;
 
     /**
+     * @var array
+     */
+    protected $params = [];
+
+    /**
      * @return static
      */
     public static function getInstance(): ?MfJsFront
@@ -33,9 +38,10 @@ class MfJsFront
      */
     public function render(array $params): string
     {
-        $tvName = $params['tvName'] ?? null;
+        $this->params = $params;
+        $tvName = $this->params['tvName'] ?? null;
         $tv = evolutionCMS()->documentObject[$tvName] ?? null;
-        $api = $params['api'] ?? null;
+        $api = $this->params['api'] ?? null;
 
         if (is_null($tv)) {
             return '';
@@ -54,13 +60,23 @@ class MfJsFront
     /**
      * @param array $data
      * @param array $config
+     * @param string $tplKey
      * @return string
      */
-    protected function renderData(array $data, array $config): string
+    protected function renderData(array $data, array $config, string $tplKey = ''): string
     {
-        print_r($data);
-        print_r($config);
-        exit;
+        $out = '';
+        foreach ($data as $item) {
+            $item['tpl'] = $config[$item['name']]['tpl'] ?? ($this->params['tpl_' . $tplKey . $item['name']] ?? '@CODE:[+value+]');
+
+            if (isset($item['items'])) {
+                $item['items'] = $this->renderData($item['items'], $config[$item['name']]['items'] ?? [], $tplKey . $item['name'] . '_');
+            }
+
+            $out .= $this->tpl($item['tpl'], $item);
+        }
+
+        return $out;
     }
 
     /**
@@ -77,6 +93,54 @@ class MfJsFront
             $config = json_decode(file_get_contents($file), true);
         }
 
-        return $config ?? [];
+        return $config['templates'] ?? [];
+    }
+
+    /**
+     * @param null $tpl
+     * @param array $plh
+     * @return string
+     */
+    protected function tpl($tpl = null, array $plh = []): string
+    {
+        $evo = evolutionCMS();
+
+        if (empty($tpl)) {
+            if (isset($plh['mf.items'])) {
+                $tpl = '@CODE:[+mf.items+]';
+            } else {
+                $tpl = '@CODE:[+value+]';
+            }
+        }
+
+        return class_exists('DLTemplate') ? DLTemplate::getInstance($evo)
+            ->parseChunk($tpl, $plh, false, true) : $evo->parseText($evo->getTpl($tpl), $plh);
+    }
+
+    /**
+     * @param string $name
+     * @param array $data
+     * @return void
+     */
+    protected function prepare(string $name = 'prepare', array &$data = [])
+    {
+        if (!empty($name)) {
+            $evo = evolutionCMS();
+
+            $args = [
+                'name' => $name,
+                'params' => [
+                    'data' => $data,
+                    'modx' => $evo,
+                    '_MFJS' => $this
+                ]
+            ];
+
+            if ((is_object($name)) || is_callable($name)) {
+                $data = call_user_func_array(...$args);
+            } else {
+                $data = $evo->runSnippet(...$args);
+            }
+        }
     }
 }
