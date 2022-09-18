cc8906c06c74f704c8fb2000a5012e72f78bbe7d
Serg <64j@mail.ru>
2022-09-18 18:44:48 +0300

    fix

--- assets/plugins/multifieldsjs/plugin.multifieldsjs.php
+++ assets/plugins/multifieldsjs/plugin.multifieldsjs.php
@@ -19,7 +19,6 @@ $evo = evolutionCMS();
 
 if ($evo->event->name == 'OnManagerMainFrameHeaderHTMLBlock') {
     if (in_array($evo->getManagerApi()->action, [3, 4, 17, 27, 72])) {
-        $evo->event->addOutput(MfJsBack::getInstance()
-            ->render());
+        $evo->event->addOutput(MfJsBack::getInstance()->render());
     }
 }
